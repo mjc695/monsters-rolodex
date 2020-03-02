f4a60bbfa33d637db005100a2f37ca2b6ea02078
Monferd Collin <monferd.j.collin@gmail.com>
2020-03-02 17:20:49 -0500

    Updates

---
+++ precache-manifest.fc86220929ddd174856f1f2d3a5d0049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55eb0d91c35fe22e7f5dfe55dfa130a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7b565cfaef5070d4f43",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c1f43cae9e3d53829993",
+    "url": "/static/js/2.438ae795.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.438ae795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b565cfaef5070d4f43",
+    "url": "/static/js/main.fc06d6fe.chunk.js"
+  },
+  {
+    "revision": "f692f6bda78da3fdfc73",
+    "url": "/static/js/runtime-main.52547352.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
