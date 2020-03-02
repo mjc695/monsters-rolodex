fa654ed5102f4a0431d82b2d4a4bf8a9658cdea5
Monferd Collin <monferd.j.collin@gmail.com>
2020-03-02 17:22:33 -0500

    Updates

---
+++ precache-manifest.69cc97751a80b5ff96b35565a24e7da7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f5a134648e2dd823c2dc3bbb2f95aa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1574555ff1a176eb1175",
+    "url": "/monsters-rolodex/static/css/main.1110f3e4.chunk.css"
+  },
+  {
+    "revision": "f261b978ad42bafc69c9",
+    "url": "/monsters-rolodex/static/js/2.2ba2f1c6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.2ba2f1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1574555ff1a176eb1175",
+    "url": "/monsters-rolodex/static/js/main.131a9a54.chunk.js"
+  },
+  {
+    "revision": "ce60410dd039e3af1545",
+    "url": "/monsters-rolodex/static/js/runtime-main.b0e96418.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc86220929ddd174856f1f2d3a5d0049.js"
+  "/monsters-rolodex/precache-manifest.69cc97751a80b5ff96b35565a24e7da7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
